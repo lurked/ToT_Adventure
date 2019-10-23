8511056ce1fa079b49b216c935342f50ca874696
Francois Boivin <lurkedd@hotmail.com>
2019-10-23 17:36:30 -0400

    v0.0.0.1

--- ToT_Adventure/Program.cs
+++ ToT_Adventure/Program.cs
@@ -13,7 +13,7 @@ namespace ToT_Adventure
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new ToT())
                 game.Run();
         }
     }
--- ToT_Adventure/ToT.cs
+++ ToT_Adventure/ToT.cs
@@ -7,12 +7,12 @@ namespace ToT_Adventure
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class ToT : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         
-        public Game1()
+        public ToT()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
